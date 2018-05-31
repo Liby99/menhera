c120468e980f60fb36d07c1031bf803ff4418455
Liby Lee <liby99@icloud.com>
2018-05-31 16:36:22 -0700

    Add parser tests

---
+++ src/compiler/grammer/parserTest.ml
@@ -0,0 +1,26 @@
+open Ast
+open AstString
+open Parsing
+open Printf
+
+let parser_tests = [
+    ("arith_1", "main { 3 - 4 + 1 }", Program([MainSect(BinOp(Plus, BinOp(Minus, Int(3), Int(4)), Int(1)))]))
+]
+
+let rec test_parsing_prog (name : string) (input : string) (expected : prog) : bool =
+    let result = parse input in
+    if result = expected
+        then true
+        else (
+            Printf.printf "Error when testing %s: \n -> expect: %s\n -> but got: %s\n" name (string_of_prog expected) (string_of_prog result);
+            failwith "Test Failed"
+        )
+
+and test_parsing_progs (tests : (string * string * prog) list) =
+    match tests with
+        | [] -> ()
+        | (n, i, e) :: rst ->
+            let _ = test_parsing_prog n i e in
+            test_parsing_progs rst
+
+and test_parsing () = test_parsing_progs parser_tests
--- src/compiler/menhera.ml
+++ src/compiler/menhera.ml
@@ -1,15 +1,3 @@
-open Parsing;;
-open AstString;;
+open ParserTest;;
 
-Printf.printf "%s\n" (string_of_prog (parse "type linked_list<T> { Tail, Node(T, linked_list<T>) }"));;
-Printf.printf "%s\n" (string_of_prog (parse "type maybe<T> { None, Some(T) }"));;
-Printf.printf "%s\n" (string_of_prog (parse "import { maybe, linked_list as ll } type a<b> { Ctor, Ctor2 }"));;
-Printf.printf "%s\n" (string_of_prog (parse "import { maybe } main { 3 + 4 }"));;
-Printf.printf "%s\n" (string_of_prog (parse "type f<T> { Call((T, T) => T) }"));;
-Printf.printf "%s\n" (string_of_prog (parse "main { let a = 1, b : int = 2, f : (int, int) => int = (x : int, y : int) : int => x + y in 3 + 4 }"));;
-Printf.printf "%s\n" (string_of_prog (parse "type maybe<T> { None, Some(T) } main { let a = Some(3) in a }"));;
-Printf.printf "%s\n" (string_of_prog (parse "main { ((a : int, b : int) : int => a + b)(3, 4) }"));;
-Printf.printf "%s\n" (string_of_prog (parse "main { (((a) => (b) => a + b)(3))(4) }"));;
-Printf.printf "%s\n" (string_of_prog (parse "main { if 3 == 4 then 4 else 5 }"));;
-Printf.printf "%s\n" (string_of_prog (parse "main { 3 + 4 - 1 }"));;
-Printf.printf "%s\n" (string_of_prog (parse "main { 3 - 4 + 1 }"));;
+test_parsing ();;
