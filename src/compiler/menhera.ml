70dcd0b70a19bacedd90a2c1f0a8c2f3cc25c401
Liby Lee <liby99@icloud.com>
2018-05-31 23:13:33 -0700

    Refined entry point

--- src/compiler/menhera.ml
+++ src/compiler/menhera.ml
@@ -1,3 +1,22 @@
-open ParserTest;;
+open Parsing
+open ParserTest
+open Printf
+open Array
 
-test_parsing ();;
+let print_help () = printf "usage: menhera [--test] [--help]\n"
+
+let main (argv : string array) : int =
+    if Array.length argv > 1 then
+        match argv.(1) with
+            | "--test"
+            | "-t" ->
+                (test_parsing (); 0)
+            | "--help"
+            | "-h" ->
+                (print_help (); 0)
+            | _ -> failwith "Not implemented"
+    else
+        (print_help (); 0)
+;;
+
+main Sys.argv
