8ce7439af17183db64b92107679e812e5edf23fc
Liby Lee <liby99@icloud.com>
2018-07-04 13:30:15 -0700

    Compiling Mini Menhera

--- mini/compile.ml
+++ mini/compile.ml
@@ -2,12 +2,14 @@ open Llvm
 open Ast
 
 let context = global_context ()
-let the_module = create_module context "program"
+let _module = create_module context "mini"
 let builder = builder context
+
+let i32_t = i32_type context
 
 let rec compile_expr (e : expr) =
     match e with
-        | EInt(i) -> const_int i32_type i
+        | EInt(i) -> const_int i32_t i
         | EBinOp(op, e1, e2) ->
             let lhs = compile_expr e1 in
             let rhs = compile_expr e2 in
@@ -19,4 +21,12 @@ let rec compile_expr (e : expr) =
 
 let rec compile_prog (p : prog) =
     match p with
-        | Program(e) -> compile_expr e
+        | Program(e) ->
+            let mainty = function_type i32_t [||] in
+            let main = declare_function "main" mainty _module in
+            let bb = append_block context "entry" main in
+            let () = position_at_end bb builder in
+            let body = compile_expr e in
+            let () = ignore (build_ret body builder) in
+            dump_value main;
+            main
--- mini/mini_menhera.ml
+++ mini/mini_menhera.ml
@@ -6,6 +6,8 @@ open Filename
 (* Parsing related *)
 open Parsing
 open AstString
+
+(* Compile related *)
 open Compile
 
 let print_help () = printf "usage: menhera [--test] [--help] [--parse FILENAME]\n"
@@ -31,8 +33,10 @@ let main (argv : string array) : int =
                     let filename = Filename.concat (Sys.getcwd ()) argv.(2) in
                     printf "Compiling File to LLVM IR: %s\n" filename;
                     let ast = Parsing.parse_file filename in
+                    printf "Generated AST: \n\t%s\n" (AstString.string_of_prog ast);
                     let ll = Compile.compile_prog ast in
-                    printf "Compiled LLVM: \n%s" (Llvm.string_of_llvalue ll);
+                    printf "Compiled LLVM: \n\t%s\n" (Llvm.string_of_llvalue ll);
+                    0
                 else failwith "Please specify file to parse"
             | _ -> failwith "Not implemented"
     else
