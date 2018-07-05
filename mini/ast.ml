f4165a8c730c3784705c5a087c2129da8d38f478
Ziyang Li <liby99@icloud.com>
2018-07-05 14:56:51 -0700

    Add more binary and unary operations for parser

--- mini/ast.ml
+++ mini/ast.ml
@@ -1,12 +1,24 @@
+type unaop =
+    | Not
+
 type binop =
     | Plus
     | Minus
+    | And
+    | Or
+    | Equal
+    | Inequal
+    | Greater
+    | GreaterOrEqual
+    | Less
+    | LessOrEqual
 
 type expr =
     | EId of string
     | EInt of int
     | EBool of bool
     | EBinOp of binop * expr * expr
+    | EUnaOp of unaop * expr
     | ELet of string * expr * expr
     | EIf of expr * expr * expr
 
