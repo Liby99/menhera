759c866f123434fe2dae5eacc35e531d5f9f297a
Liby Lee <liby99@icloud.com>
2018-07-13 17:09:50 -0700

    Unified Style

--- src/runner/runner.ml
+++ src/runner/runner.ml
@@ -5,7 +5,7 @@ open PosixTypes
 open Foreign
 
 let run (m : llmodule) (name : string) : int =
-    (ignore (initialize ()));
-    let engine = create m in
-    let func = get_function_address name (funptr (void @-> returning int)) engine in
-    func ()
+  (ignore (initialize ()));
+  let engine = create m in
+  let func = get_function_address name (funptr (void @-> returning int)) engine in
+  func ()
