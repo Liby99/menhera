c61ecdfb5cfffadb3685db4847fc6e30b157c8a3
Liby Lee <liby99@icloud.com>
2018-08-29 11:27:21 -0700

    Moved extract functions

--- src/core/mhrContext.js
+++ src/core/mhrContext.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 const Parser = require('parser/parser');
-const Preprocessor = require('preprocessor/preprocessor');
+const MhrFunction = require('core/mhrFunction');
+const MhrNode = require('core/mhrNode');
 
 class MhrContext {
   constructor(filename) {
@@ -14,7 +15,7 @@ class MhrContext {
     this.ast = Parser.parse(this.file);
     
     // Preprocessing - get main expression and functions
-    this.functions = Preprocessor.extractFunctions(this.ast);
+    this.functions = MhrContext.extractFunctions(this.ast);
   }
   
   getFileName() {
@@ -31,6 +32,51 @@ class MhrContext {
   
   getFunctions() {
     return this.functions;
+  }
+  
+  static extractFunctions(ast) {
+    
+    // Setup functions array
+    const functions = {};
+    
+    // Traverse the ast, extract the visible function and return the processed ast
+    const traverse = (node, env) => node.match({
+      'bin_op': ({ type, e1, op, e2 }) => new MhrNode({
+        type,
+        op,
+        e1: traverse(e1, env),
+        e2: traverse(e2, env)
+      }),
+      'let': ({ type, name, binding, expr }) => new MhrNode({
+        type,
+        name,
+        binding: traverse(binding, env),
+        expr: traverse(expr, env)
+      }),
+      'application': ({ type, callee, params }) => new MhrNode({
+        type,
+        callee: traverse(callee, env),
+        params: params.map((param) => traverse(param, env))
+      }),
+      'function': ({ args, body }) => {
+        const name = MhrFunction.generateName();
+        const newBody = traverse(body, name);
+        const f = new MhrFunction(args, newBody, env, name);
+        functions[name] = f;
+        return new MhrNode({
+          type: 'closure',
+          func: f
+        });
+      },
+      '_': (node) => node,
+    });
+    
+    // Get the main function
+    const mainExpr = traverse(ast.getRootNode(), 'main');
+    functions['main'] = new MhrFunction([], mainExpr, undefined, 'main');
+    
+    // Return functions
+    return functions;
   }
 }
 
