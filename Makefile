31a77456b88505c49f3b9a58e72fad8ec80ebae1
Liby Lee <liby99@icloud.com>
2018-08-29 22:25:49 -0700

    Done including submodule

--- Makefile
+++ Makefile
@@ -3,6 +3,9 @@ all: build_tree_sitter_menhera
 	cp script/menhera.js ./menhera
 	chmod +x ./menhera
 
+update_submodule:
+	git submodule update --recursive --remote
+
 build_tree_sitter_menhera:
 	make -C include/tree-sitter-menhera
 
--- src/parser/parser.ts
+++ src/parser/parser.ts
@@ -1,5 +1,5 @@
 import * as TreeSitter from 'tree-sitter';
-import * as Menhera from '../../lib/tree-sitter-menhera/index';
+import * as Menhera from '../../include/tree-sitter-menhera/index';
 import FileContext from '../parser/fileContext';
 import MhrAst from '../core/mhrAst';
 import MhrNode from '../core/mhrNode';
