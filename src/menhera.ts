6e611334009424b23ea8506734e10d2217372630
Liby Lee <liby99@icloud.com>
2018-09-22 14:34:04 -0400

    Can infer a lot of things

--- src/menhera.ts
+++ src/menhera.ts
@@ -1,9 +1,7 @@
 import MhrContext from 'core/mhrContext';
-import print from 'utility/print';
 import compile from 'compiler/compile';
 
 ((filename: string): void => {
   const context = new MhrContext(filename);
-  // print(context.functions);
   compile(context);
 })(process.argv[2]);
